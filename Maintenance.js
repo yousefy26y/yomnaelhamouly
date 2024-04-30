980b1b2288d3c7eb85a1d2d132148cfe5a826fcf
Yousef Ashraf <146857668+SoSiime@users.noreply.github.com>
2024-04-30 22:47:04 +0300

    Maintenance.js

--- Maintenance.js
+++ Maintenance.js
@@ -1,4 +1,4 @@
-function MaintenanceCheck() {
+ function MaintenanceCheck() {
             let FirstName = document.forms["MaintenanceInfo"]["FirstName"].value;
             let LastName = document.forms["MaintenanceInfo"]["LastName"].value;
             let MobileNumber = document.forms["MaintenanceInfo"]["MobileNumber"].value;
@@ -8,4 +8,4 @@ function MaintenanceCheck() {
                 alert("You must fill out all fields in the form");
                 return false;
              }
-        }
+}
